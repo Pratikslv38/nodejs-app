526ec0ae87c33d1105782c1424b8eb85aebea5c5
Pratikslv38 <100276171+Pratikslv38@users.noreply.github.com>
2022-09-12 10:27:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/mithuntechnologies', function(request, response) {
     //response.send("<h2><center>Welcome to Node JS app</h2>");
     response.write("<h2><center><u>Node JS  Application </u></center></h2>");
 	
-    response.write("<h2><center>Welcome to  Mithun Technologies. Please Contact +91-9980923226,++91-9980923216 for more information or send an email to devopstrainingblr@gmail.com <center></h2>" );
+    response.write("<h2><center>Welcome to  Valueadd SoftTech<center></h2>" );
     response.end();
     
   })
@@ -44,10 +44,8 @@ app.get("/html", function(req,res){
         //res.type('application/json');
         //res.json({'name': 'Mithun Reddy L'});
         res.send({
-		'name': 'Mithun Technologies',
+		'name': 'Pratik',
 		'technology': 'DevOps',
-		'contact' : '9980923226',
-		'email': 'devopstrainingblr@gmail.com'
 	            });
         
         });
@@ -67,15 +65,10 @@ app.get("/status-code-500", function(req, res) {
     //res.sendStatus(500);
    res.status(500).send('Internal Server Error – custom message');
 })
-
-app.get('/redirect', function(req, res) {
-    //Send status 300
-        res.redirect('http://mithuntechnologies.com');
-    });
     
  
     app.listen(app.get('port'), function() {
-        console.log("Node JS app is running at http://localhost:" + app.get('port') +"/mithuntechnologies");
+        console.log("Node JS app is running at http://localhost:" + app.get('port'));
       })
     
 
